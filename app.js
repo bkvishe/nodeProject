81893e5d6596bf5415eb796c9d2cdfb68a725f7c
Bharat Vishe <bharat.vishe@gmail.com>
2017-06-24 20:25:31 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var http = require("http");
+var port = 9000;
+
+http.createServer(function(req, resp){
+
+    resp.writeHead(200, {"Content-type":"text/html"});
+    resp.write("<html><b>Ok</b></html>");
+    resp.end();
+}).listen(port);
