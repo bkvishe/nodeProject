f75f62f84971b6a944e3c668096c6b27aa0ffae6
Bharat Vishe <bharat.vishe@gmail.com>
2017-06-25 11:47:54 +0530

    added new files

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-var http = require("http");
-var port = 9000;
+// Copyright 2017, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-http.createServer(function(req, resp){
+'use strict';
 
-    console.log("Server is listening to port 9000 !");
-    resp.writeHead(200, {"Content-type":"text/html"});
-    resp.write("<html><b>Ok</b></html>");
-    resp.end();
+const express = require('express');
 
-}).listen(port);
+const app = express();
+
+// [START hello_world]
+// Say hello!
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!');
+});
+// [END hello_world]
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  const server = app.listen(process.env.PORT || 8081, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
+  });
+  // [END server]
+}
+
+module.exports = app;
